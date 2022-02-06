95a0500301338917879f612c2187c66d4d45ce54
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-02-05 22:36:25 -0600

    fix err msg

--- scripts/fusion.js
+++ scripts/fusion.js
@@ -21,9 +21,9 @@ const correctChain = 1;
 /*********************************END CONFIG************************************/
 
 if (window.ethereum == undefined) {
-    displayErrorMessage('Use a web3 enabled browser with MetaMask or the MetaMask mobile app to claim $PLASMA!');
-    $("#available-yeti-images").empty();
-    $("#available-yeti-images").append("<br><p>No yetis available...</p>");
+    displayErrorMessage('Use a web3 enabled browser with MetaMask or the MetaMask mobile app to fuse Cyborgs!');
+    $("#available-cyborg-images").empty();
+    $("#available-cyborg-images").append("<br><p>No cyborgs available...</p>");
 }
 
 const provider = new ethers.providers.Web3Provider(window.ethereum,"any");
